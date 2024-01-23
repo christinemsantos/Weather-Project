773fd941b62e2e6376e40e0e795fe5a22dcc2e08
Christine Santos <56892403+christinemsantos@users.noreply.github.com>
2024-01-23 13:08:48 -0500

    rearranged divs to improve appearance

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+function displayWeather(response) {
+  console.log(response);
+}
+
+function searchCity(city) {
+  let apiKey = "af51t24f57b6fo0d3407bf1a846aaa92";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeather);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let city = document.querySelector("#city");
   city.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
   searchInput.value = "";
 }
 
